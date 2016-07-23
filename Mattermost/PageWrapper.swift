11fca6314a98f373e50e16849037ac0202fe5396
Maxim Gubin <maxim@kilograpp.com>
2016-07-24 00:43:40 +0300

    Adding PageWrapper

--- Mattermost/PageWrapper.swift
+++ Mattermost/PageWrapper.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class PageWrapper: NSObject {
+    let page: Int
+    let size: Int
+    let channel: Channel
+    let lastPostId: String?
+    
+    init(page: Int = 0, size: Int = 60, channel: Channel, lastPostId: String? = nil) {
+        self.page = page
+        self.size = size
+        self.channel = channel
+        self.lastPostId = lastPostId
+    }
+}
+
+public enum PageWrapperAttributes: String {
+    case channel = "channel"
+    case page = "page"
+    case size = "size"
+    case lastPostId = "lastPostId"
+}
