e5025267826a48069f8641e2cbaa8944317cffef
Maxim Gubin <maxim@kilograpp.com>
2016-07-26 21:53:22 +0300

    Adding String+JsonSerialization extension

---
+++ Mattermost/extensions/String+JsonSerialization.swift
@@ -0,0 +1,22 @@
+//
+//  String+JsonSerialization.swift
+//  Mattermost
+//
+//  Created by Maxim Gubin on 26/07/16.
+//  Copyright © 2016 Kilograpp. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func toDictionary() -> [String:AnyObject]? {
+        if let data = self.dataUsingEncoding(NSUTF8StringEncoding) {
+            do {
+                return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String:AnyObject]
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+        return nil
+    }
+}
