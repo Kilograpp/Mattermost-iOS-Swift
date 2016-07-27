d781efc17ee96921907aee55043f6b889d3f9f28
Maxim Gubin <maxim@kilograpp.com>
2016-07-27 11:55:07 +0300

    Adding Json serialisation method to NSString

--- Mattermost/extensions/String+JsonSerialization.swift
+++ Mattermost/extensions/String+JsonSerialization.swift
@@ -20,3 +20,16 @@ extension String {
         return nil
     }
 }
+
+extension NSString {
+    func toDictionary() -> [String:AnyObject]? {
+        if let data = self.dataUsingEncoding(NSUTF8StringEncoding) {
+            do {
+                return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String:AnyObject]
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+        return nil
+    }
+}
