c4016280497c36515db4c5c566576af8c9b6b90b
Maxim Gubin <maxim@kilograpp.com>
2016-08-01 06:14:32 +0300

    Adding files logic

--- Mattermost/model/Human/File.swift
+++ Mattermost/model/Human/File.swift
@@ -7,9 +7,136 @@
 //
 
 import Foundation
+import RealmSwift
 
+private protocol Interface: class {
+    func downloadURL() -> NSURL?
+    func thumbURL() -> NSURL?
+}
 
+final class File: RealmObject {
+    dynamic var name: String?
+    dynamic var isImage: Bool = false
+    dynamic var _downloadLink: String?
+    dynamic var _thumbLink: String?
+    dynamic var rawLink: String? {
+        didSet {
+            computeName()
+            computeDownloadLink()
+            computeThumbLink()
+            computeIsImage()
+        }
+    }
+    let _posts = LinkingObjects(fromType: Post.self, property: PostRelationships.files.rawValue)
+    var post: Post?  {
+        return _posts.first
+    }
+}
 
-class File: RealmObject {
+enum FileAttributes: String {
+    case isImage = "isImage"
+    case rawLink = "rawLink"
+    case name = "name"
+}
+
+enum FileRelationships: String {
+    case post = "post"
+}
+private protocol PathPatterns: class {
+    static func downloadPathPattern() -> String
+    static func thumbPathPattern() -> String
+    static func updateCommonPathPattern() -> String
+}
+
+private protocol ResponseMappings: class {
+    static func simplifiedMapping() -> RKObjectMapping
+}
+
+private protocol Computations: class {
+    func computeName()
+    func computeDownloadLink()
+    func computeThumbLink()
+    func computeIsImage()
+}
+
+private protocol ResponseDescriptors: class {
+    static func updateResponseDescriptor() -> RKResponseDescriptor
+}
+
+private protocol Support: class {
+    func thumbPostfix() -> String?
+    static func teamIdentifierPath() -> String
+}
+
+extension File: PathPatterns {
+    static func downloadPathPattern() -> String {
+        return "teams/:\(teamIdentifierPath())/files/get_info:\(FileAttributes.rawLink)"
+    }
+    static func thumbPathPattern() -> String {
+        return "teams/:\(teamIdentifierPath())/files/get:thumbPostfix\\.jpg"
+    }
+    static func updateCommonPathPattern() -> String {
+        return "teams/:path/files/get_info/:path/:path/:path/:path"
+    }
+}
+
+extension File: ResponseMappings {
+    static func simplifiedMapping() -> RKObjectMapping {
+        let mapping = super.emptyMapping()
+        mapping.addPropertyMapping(RKAttributeMapping(fromKeyPath: nil, toKeyPath: FileAttributes.rawLink.rawValue))
+        return mapping
+    }
+}
+
+extension File: ResponseDescriptors {
+    static func updateResponseDescriptor() -> RKResponseDescriptor {
+        return RKResponseDescriptor(mapping: mapping(),
+                                    method: .GET,
+                                    pathPattern: updateCommonPathPattern(),
+                                    keyPath: nil,
+                                    statusCodes:  RKStatusCodeIndexSetForClass(.Successful))
+    }
+}
+
+extension File: Computations {
+    private func computeName() {
+        let components = self.rawLink?.componentsSeparatedByString("/")
+        if let components = components where components.count >= 2 {
+            let fileName = components.last!.stringByRemovingPercentEncoding
+            self.name = fileName
+        } else {
+            self.name = rawLink
+        }
+    }
     
+    private func computeDownloadLink() {
+        self._downloadLink = FileUtils.downloadLinkForFile(self)?.absoluteString
+    }
+    
+    private func computeThumbLink() {
+        self._thumbLink = FileUtils.thumbLinkForFile(self)?.absoluteString
+    }
+    
+    private func computeIsImage() {
+        self.isImage = FileUtils.fileIsImage(self)
+    }
+}
+
+extension File: Support {
+    func thumbPostfix() -> String? {
+        return FileUtils.thumbPostfixForInternalFile(self)
+    }
+    static func teamIdentifierPath() -> String {
+        return "\(FileRelationships.post).\(PostRelationships.channel).\(ChannelRelationships.team).\(TeamAttributes.identifier)"
+    }
+    
+}
+
+extension File: Interface {
+    func downloadURL() -> NSURL? {
+        return NSURL(string: self._downloadLink ?? StringUtils.emptyString())
+    }
+    func thumbURL() -> NSURL? {
+        return NSURL(string: self._thumbLink ?? StringUtils.emptyString())
+    }
 }
