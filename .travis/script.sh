5a7b63e5a456a635ca12952424f716e2e4015d46
Maxim Gubin <maxim@kilograpp.com>
2016-08-29 01:37:39 +0300

    Script fixes

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_BRANCH" == "development" ]]; then
+if [[ "$TRAVIS_BRANCH" == "development" ]]
+    bundle exec pod setup
     bundle exec pod repo update 
     fastlane beta
     exit $?
 else
+    bundle exec pod setup
     bundle exec pod repo update
     fastlane release
     exit $?
