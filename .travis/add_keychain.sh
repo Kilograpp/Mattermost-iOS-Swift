1e3ba6e6f762b2d8095d1aed1b0b6c4de4cec41a
Maxim Gubin <maxim@kilograpp.com>
2016-08-31 23:58:30 +0300

    Keychain edited

--- .travis/add_keychain.sh
+++ .travis/add_keychain.sh
@@ -2,7 +2,9 @@
 
 KEYCHAIN_PATH=.travis/travis.keychain
 
-security list-keychains -s "$KEYCHAIN_PATH"
+security list-keychains -s "$PWD/$KEYCHAIN_PATH"
 security default-keychain -s "$PWD/$KEYCHAIN_PATH"
 security unlock-keychain -p $MATCH_PASSWORD "$PWD/$KEYCHAIN_PATH"
 
+
+security list-keychains
