1e0efcab205d76b16c75c245f2d7c0cfd0c1e8e6
Maxim Gubin <maxim@kilograpp.com>
2016-09-01 01:39:01 +0300

    Code sign issues fixing

--- .travis/add_keychain.sh
+++ .travis/add_keychain.sh
@@ -8,12 +8,6 @@ security default-keychain -s $KEYCHAIN
 security unlock-keychain -p $PASSWORD $KEYCHAIN
 security set-keychain-settings -t 10000 -u $KEYCHAIN
 
-security import .travis/apple.cer -k $KEYCHAIN -T /usr/bin/codesign
-security import .travis/dist.cer -k $KEYCHAIN -T /usr/bin/codesign
-security import .travis/dist.p12 -k $KEYCHAIN -P $MATCH_PASSWORD -T /usr/bin/codesign
-
-security list-keychains -s $KEYCHAIN
-security list-keychains
-
-security default-keychain -s $KEYCHAIN
-security unlock-keychain -p $PASSWORD $KEYCHAIN
+security import .travis/apple.cer -k ~/Library/Keychains/$KEYCHAIN -T /usr/bin/codesign
+security import .travis/dist.cer -k ~/Library/Keychains/$KEYCHAIN -T /usr/bin/codesign
+security import .travis/dist.p12 -k ~/Library/Keychains/$KEYCHAIN -P $MATCH_PASSWORD -T /usr/bin/codesign
