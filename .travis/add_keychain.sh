0ff609f87666fb4c68e2db0489c418be2356955c
Maxim Gubin <maxim@kilograpp.com>
2016-08-31 16:58:54 +0300

    Modifying code sign

--- .travis/add_keychain.sh
+++ .travis/add_keychain.sh
@@ -4,6 +4,10 @@ KEYCHAIN=ios-build.keychain
 security create-keychain -p travis $KEYCHAIN
 security default-keychain -s $KEYCHAIN
 security unlock-keychain -p travis $KEYCHAIN
-security set-keychain-settings -t 5000 $KEYCHAIN
-security import .travis/apple.cer -k ~/Library/Keychains/$KEYCHAIN -T /usr/bin/codesign
-security import .travis/dist.p12 -k ~/Library/Keychains/$KEYCHAIN -P $MATCH_PASSWORD -T /usr/bin/codesign
+security set-keychain-settings -t 10000 $KEYCHAIN
+security import ./travis/apple.cer -k ~/Library/Keychains/$KEYCHAIN -T /usr/bin/codesign
+security import ./travis/dist.p12 -k ~/Library/Keychains/$KEYCHAIN -P $MATCH_PASSWORD -T /usr/bin/codesign
+
+security list-keychains
+
+security find-identity -p codesigning ~/Library/Keychains/$KEYCHAIN
