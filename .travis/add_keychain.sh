14b05d78500447f94ab63fef086558a752c57853
Maxim Gubin <maxim@kilograpp.com>
2016-08-31 17:10:37 +0300

    Travis fixes

--- .travis/add_keychain.sh
+++ .travis/add_keychain.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+ls
+
 KEYCHAIN=ios-build.keychain
 security create-keychain -p travis $KEYCHAIN
 security default-keychain -s $KEYCHAIN
 security unlock-keychain -p travis $KEYCHAIN
 security set-keychain-settings -t 10000 -u $KEYCHAIN
-security import ./travis/apple.cer -k ~/Library/Keychains/$KEYCHAIN -T /usr/bin/codesign
-security import ./travis/dist.p12 -k ~/Library/Keychains/$KEYCHAIN -P $MATCH_PASSWORD -T /usr/bin/codesign
+security import apple.cer -k ~/Library/Keychains/$KEYCHAIN -T /usr/bin/codesign
+security import dist.p12 -k ~/Library/Keychains/$KEYCHAIN -P $MATCH_PASSWORD -T /usr/bin/codesign
 
 security list-keychains
 
