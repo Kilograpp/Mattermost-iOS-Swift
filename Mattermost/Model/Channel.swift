09089d80b232a41bf4bde09ad6d839b5ade62f12
jufina <nightrina@gmail.com>
2016-10-10 20:45:14 +0300

    Fixed MM-172

--- Mattermost/Model/Channel.swift
+++ Mattermost/Model/Channel.swift
@@ -120,7 +120,7 @@ extension Channel: Support {
     }
     
     func hasNewMessages() -> Bool {
-        guard lastViewDate != nil else { return true }
+        guard lastViewDate != nil else { return false }
       return ((self.lastViewDate as NSDate?)?.isEarlierThan(self.lastPostDate))!
     }
 }
