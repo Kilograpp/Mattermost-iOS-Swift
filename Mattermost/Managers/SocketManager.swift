92b17d7fffd9f2b55d1ca6f7f7c1d77a22a7e2cd
jufina <nightrina@gmail.com>
2016-10-12 15:21:35 +0300

    MM-168 fixing deleting,updating and creating own posts

--- Mattermost/Managers/SocketManager.swift
+++ Mattermost/Managers/SocketManager.swift
@@ -159,22 +159,23 @@ extension SocketManager: Notifications {
     
     func handleReceivingNewPost(_ channelId:String,channelName:String,channelType:String,senderName:String,post:Post) {
         // if user is not author
-        if post.authorId != Preferences.sharedInstance.currentUserId {
+        if !postExistsWithIdentifier(post.identifier!, pendingIdentifier: post.localIdentifier!) {
             RealmUtils.save(post)
         }
     }
     
     func handleReceivingUpdatedPost(_ updatedPost:Post) {
-        // if user is not author
-        if updatedPost.authorId != Preferences.sharedInstance.currentUserId {
-            RealmUtils.save(updatedPost)
+        if postExistsWithIdentifier(updatedPost.identifier!, pendingIdentifier: updatedPost.localIdentifier!) {
+            let existedPost = RealmUtils.realmForCurrentThread().objects(Post.self).filter("%K == %@", PostAttributes.identifier.rawValue, updatedPost.identifier!).first!
+            updatedPost.localIdentifier = existedPost.localIdentifier
         }
+        RealmUtils.save(updatedPost)
     }
     
     func handleReceivingDeletedPost(_ deletedPost:Post) {
         // if user is not author
         let day = deletedPost.day
-        if deletedPost.authorId != Preferences.sharedInstance.currentUserId {
+        if postExistsWithIdentifier(deletedPost.identifier!, pendingIdentifier: deletedPost.localIdentifier!) {
             let post = RealmUtils.realmForCurrentThread().objects(Post.self).filter("%K == %@", "identifier", deletedPost.identifier!).first
             guard post != nil else { return }
             RealmUtils.deleteObject(post!)
@@ -227,11 +228,13 @@ extension SocketManager: Notifications {
     fileprivate func handleUserJoined(user: User, channel: Channel) {
         try! RealmUtils.realmForCurrentThread().write {
             channel.members.append(user)
-            channel.currentUserInChannel = true
         }
         
-        //Если это мы присоединились -> обновить данные в левом меню
+        //If joined current user -> reload left menu
         let notificationName = Constants.NotificationsNames.UserJoinNotification
+        try! RealmUtils.realmForCurrentThread().write {
+            channel.currentUserInChannel = true
+        }
         if user.identifier == Preferences.sharedInstance.currentUserId {
             NotificationCenter.default.post(name: Notification.Name(rawValue: notificationName), object: channel)
         }
@@ -262,7 +265,7 @@ extension SocketManager: StateControl {
 extension SocketManager: Validation {
     fileprivate func postExistsWithIdentifier(_ identifier: String, pendingIdentifier: String) -> Bool {
         let realm = try! Realm()
-        let predicate = NSPredicate(format: "%K == %@ || %K == %@", PostAttributes.identifier.rawValue, identifier, PostAttributes.pendingId.rawValue, pendingIdentifier)
+        let predicate = NSPredicate(format: "%K == %@ || %K == %@", PostAttributes.identifier.rawValue, identifier, PostAttributes.localId.rawValue, pendingIdentifier)
         return realm.objects(Post).filter(predicate).first != nil
     }
 }
