de5f923524cb77b51d342c510f5fc14074b3cbe6
TaHyKu <tahyku@MacBook-Pro-TaHyKu.local>
2016-10-15 23:11:28 +0300

    Fixed MM-187

--- Mattermost/Views/TableSectionHeaders/LeftMenu/LeftMenuSectionFooter.swift
+++ Mattermost/Views/TableSectionHeaders/LeftMenu/LeftMenuSectionFooter.swift
@@ -51,6 +51,7 @@ extension LeftMenuSectionFooter : PrivateSetup {
         self.titleButton.setImage(UIImage(named: "common_arrow_icon_passive"), for: UIControlState())
         self.titleButton.titleLabel?.font = FontBucket.footerTitleFont
         self.titleButton.setTitle("more ", for: UIControlState())
+        self.titleButton.setTitleColor(ColorBucket.sideMenuCommonTextColor, for: .normal)
         self.titleButton.transform = CGAffineTransform(scaleX: -1.0, y: 1.0);
         self.titleButton.titleLabel!.transform = CGAffineTransform(scaleX: -1.0, y: 1.0);
         self.titleButton.imageView!.transform = CGAffineTransform(scaleX: -1.0, y: 1.0);
