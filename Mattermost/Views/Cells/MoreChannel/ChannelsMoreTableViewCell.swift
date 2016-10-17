4a19a7d0367b5cce0737603bf59060b9c0c6d420
TaHyKu <tahyku@MacBook-Pro-TaHyKu.local>
2016-10-17 11:16:16 +0300

    Tech commit

---
+++ Mattermost/Views/Cells/MoreChannel/ChannelsMoreTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChannelsMoreTableViewCell.swift
+//  Mattermost
+//
+//  Created by TaHyKu on 17.10.16.
+//  Copyright © 2016 Kilograpp. All rights reserved.
+//
+
+import UIKit
+
+class ChannelsMoreTableViewCell: TableViewCell {
+
+}
