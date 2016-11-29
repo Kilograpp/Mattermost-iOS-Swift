299a24967fe73bfa0b42cbd02c102d2f9469f0b8
Сергей <sergey_kononov@kilograpp.com>
2016-11-29 11:12:36 +0300

    mm-510

--- Mattermost/Views/Cells/Chat/AttachmentImageCell.swift
+++ Mattermost/Views/Cells/Chat/AttachmentImageCell.swift
@@ -72,7 +72,7 @@ final class AttachmentImageCell: UITableViewCell, Reusable, Attachable {
         if let image = SDImageCache.shared().imageFromMemoryCache(forKey: downloadUrl.absoluteString) {
             self.fileImageView.image = image
         } else {
-            self.fileImageView.image = nil
+            self.fileImageView.image = UIImage(named: "image_back")
             let imageDownloadCompletionHandler: SDWebImageCompletionWithFinishedBlock = {
                 [weak self] (image, error, cacheType, isFinished, imageUrl) in
                 DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated).async {
