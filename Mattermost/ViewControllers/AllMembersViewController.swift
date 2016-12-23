9e4450e4cfa4729309d6b7e0d29d82dbc6baf97c
desubro@kilograpp.com <desubro@kilograpp.com>
2016-12-23 13:27:50 +0300

    AllMembersViewController refactoring

--- Mattermost/ViewControllers/AllMembersViewController.swift
+++ Mattermost/ViewControllers/AllMembersViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class AllMembersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating   {
+class AllMembersViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     var searchController: UISearchController!
@@ -21,63 +21,39 @@ class AllMembersViewController: UIViewController, UITableViewDelegate, UITableVi
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        membersList = channel.members.sorted(byProperty: UserAttributes.username.rawValue)
-        tableView.tableFooterView = UIView.init(frame: CGRect.zero)
-        tableView.dataSource = self
-        tableView.delegate = self
+        initialSetup()
+    }
+}
+
+fileprivate protocol Setup {
+    func initialSetup()
+    func setupNavigationBar()
+    func setupSearchBar()
+    func setupTableView()
+}
+
+fileprivate protocol Action {
+    func backAction()
+}
+
+//MARK: Setup
+extension AllMembersViewController: Setup {
+    func initialSetup(){
         setupNavigationBar()
         setupSearchBar()
-        
+        setupTableView()
+        membersList = channel.members.sorted(byProperty: UserAttributes.username.rawValue)
         let nib = UINib(nibName: "MemberChannelSettingsCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "memberChannelSettingsCell")
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupTableView() {
+        tableView.tableFooterView = UIView.init(frame: CGRect.zero)
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return nil
-    }
-    
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return searchController.isActive ? searchMembersList.count : membersList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell: UITableViewCell!
-        let memberCell = tableView.dequeueReusableCell(withIdentifier: "memberChannelSettingsCell") as! MemberChannelSettingsCell
-        let member = searchController.isActive ? searchMembersList[indexPath.row] : membersList[indexPath.row]
-        memberCell.configureWithUser(user: member)
-        cell = memberCell
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 1.0
-    }
-    
     func setupNavigationBar() {
         self.title = "All Members".localized
-        /*
-         let backButton = UIBarButtonItem.init(image: UIImage(named: "navbar_back_icon"), style: .done, target: self, action: #selector(backAction))
-         self.navigationItem.leftBarButtonItem = backButton*/
         self.navigationItem.backBarButtonItem?.title = ""
-    }
-    
-    func backAction(){
-        _=self.navigationController?.popViewController(animated: true)
     }
     
     func setupSearchBar(){
@@ -107,6 +83,67 @@ class AllMembersViewController: UIViewController, UITableViewDelegate, UITableVi
         searchController.searchResultsUpdater = self
         searchController.dimsBackgroundDuringPresentation = false
     }
+}
+
+//MARK: Search updating
+extension AllMembersViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            filterContent(searchText: searchText)
+            tableView.reloadData()
+        }
+    }
+    
+    func filterContent(searchText: String){
+        
+        let membersIdentifiers = Array(membersList.map{$0.identifier!})
+        let sortName = UserAttributes.username.rawValue
+        
+        let predicate =  NSPredicate(format: "username CONTAINS[c] '\(searchText)' AND identifier IN %@", membersIdentifiers)
+        searchMembersList = RealmUtils.realmForCurrentThread().objects(User.self).filter(predicate).sorted(byProperty: sortName)
+    }
+}
+
+//MARK: UITableViewDataSource
+extension AllMembersViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return nil
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return searchController.isActive ? searchMembersList.count : membersList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell!
+        let memberCell = tableView.dequeueReusableCell(withIdentifier: "memberChannelSettingsCell") as! MemberChannelSettingsCell
+        let member = searchController.isActive ? searchMembersList[indexPath.row] : membersList[indexPath.row]
+        memberCell.configureWithUser(user: member)
+        cell = memberCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 1.0
+    }
+}
+
+//MARK: UITableViewDelegate
+extension AllMembersViewController: UITableViewDelegate {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard self.lastSelectedIndexPath == nil else { return }
@@ -114,7 +151,7 @@ class AllMembersViewController: UIViewController, UITableViewDelegate, UITableVi
         
         let member = searchController.isActive ? searchMembersList[indexPath.row] : membersList[indexPath.row]
         self.searchController.isActive = false
-
+        
         if member.identifier == Preferences.sharedInstance.currentUserId!{
             self.lastSelectedIndexPath = nil
             return
@@ -134,21 +171,12 @@ class AllMembersViewController: UIViewController, UITableViewDelegate, UITableVi
             })
         }
     }
-    
-    //Search updating
-    func updateSearchResults(for searchController: UISearchController) {
-        if let searchText = searchController.searchBar.text {
-            filterContent(searchText: searchText)
-            tableView.reloadData()
-        }
-    }
-    
-    func filterContent(searchText: String){
-        
-        let membersIdentifiers = Array(membersList.map{$0.identifier!})
-        let sortName = UserAttributes.username.rawValue
+}
 
-        let predicate =  NSPredicate(format: "username CONTAINS[c] '\(searchText)' AND identifier IN %@", membersIdentifiers)
-        searchMembersList = RealmUtils.realmForCurrentThread().objects(User.self).filter(predicate).sorted(byProperty: sortName)
+//MARK: Action
+extension AllMembersViewController: Action {
+    func backAction(){
+        _=self.navigationController?.popViewController(animated: true)
     }
 }
+
