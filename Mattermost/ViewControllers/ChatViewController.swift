aeead070e3c7e819ab64b5455274f6b48c6dc76a
TaHyKu <tahyku@MacBook-Pro-TaHyKu.local>
2017-01-19 23:51:57 +0200

    Fixed files downloading

--- Mattermost/ViewControllers/ChatViewController.swift
+++ Mattermost/ViewControllers/ChatViewController.swift
@@ -1039,3 +1039,64 @@ extension ChatViewController: AttachmentsModuleDataSource {
         return self.channel
     }
 }
+
+
+
+
+
+///////////////////////////
+
+
+//MARK: ChatViewController
+extension ChatViewController {
+    func presentDocumentInteractionController(notification: NSNotification) {
+        let fileId = notification.userInfo?["fileId"]
+        let file = RealmUtils.realmForCurrentThread().object(ofType: File.self, forPrimaryKey: fileId)
+        let filePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "/" + (file?.name)!
+        
+        print(filePath)
+        
+        if FileManager.default.fileExists(atPath: filePath) {
+            self.documentInteractionController = UIDocumentInteractionController(url: URL(fileURLWithPath: filePath))
+            self.documentInteractionController?.delegate = self
+            self.documentInteractionController?.presentPreview(animated: true)
+        }
+    }
+}
+
+
+//MARK: UIDocumentInteractionControllerDelegate
+extension ChatViewController: UIDocumentInteractionControllerDelegate {
+    func documentInteractionController(_ controller: UIDocumentInteractionController, willBeginSendingToApplication application: String?) {
+    }
+    func documentInteractionController(_ controller: UIDocumentInteractionController, didEndSendingToApplication application: String?) {
+    }
+    func documentInteractionControllerDidDismissOpenInMenu(_ controller: UIDocumentInteractionController) {
+    }
+    func documentInteractionControllerDidDismissOptionsMenu(_ controller: UIDocumentInteractionController) {
+    }
+    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+        return self
+    }
+}
+
+
+//MARK: ImagesPreviewViewController
+extension ChatViewController {
+    func openPreviewWith(postLocalId: String, fileId: String) {
+        let last = self.navigationController?.viewControllers.last
+        guard last != nil else { return }
+        guard !(last?.isKind(of: ImagesPreviewViewController.self))! else { return }
+        
+        let gallery = self.storyboard?.instantiateViewController(withIdentifier: "ImagesPreviewViewController") as! ImagesPreviewViewController
+        
+        gallery.configureWith(postLocalId: postLocalId, initalFileId: fileId)
+        let transaction = CATransition()
+        transaction.duration = 0.5
+        transaction.timingFunction = CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseInEaseOut)
+        transaction.type = kCATransitionMoveIn
+        transaction.subtype = kCATransitionFromBottom
+        self.navigationController!.view.layer.add(transaction, forKey: kCATransition)
+        self.navigationController?.pushViewController(gallery, animated: false)
+    }
+}
