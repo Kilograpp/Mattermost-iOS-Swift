27824f91cf3f5498064803cdc5d2c6f22bfc8029
TaHyKu <tahyku@MacBook-Pro-TaHyKu.local>
2017-02-03 02:21:55 +0200

    Fixed bugs with user avatars

--- Mattermost/Views/Cells/Chat/MemberLinkTableViewCell.swift
+++ Mattermost/Views/Cells/Chat/MemberLinkTableViewCell.swift
@@ -13,29 +13,31 @@ class MemberLinkTableViewCell: UITableViewCell {
     @IBOutlet weak var memberImage: UIImageView!
     @IBOutlet weak var memberFullName: UILabel!
     @IBOutlet weak var memberNickname: UILabel!
+    
+    fileprivate var userId: String?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+    
         memberImage.layer.cornerRadius = 10.0
         memberImage.clipsToBounds = true
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
     func configureWithUser(user: User) {
-        memberNickname.text = "@"+user.username!
+        let userId = user.identifier
+        self.userId = userId
+        
+        memberNickname.text = "@" + user.username!
         memberImage.image = UIImage.sharedAvatarPlaceholder
         ImageDownloader.downloadFeedAvatarForUser(user) { [weak self] (image, error) in
+            guard self?.userId == userId else { return }
             self?.memberImage.image = image
         }
-        memberFullName.text = user.firstName!+" "+user.lastName!
+        memberFullName.text = user.firstName! + " " + user.lastName!
     }
     
     func configureWithIndex(index: Int) {
-        memberNickname.text = "@"+Constants.LinkCommands.name[index]
+        memberNickname.text = "@" + Constants.LinkCommands.name[index]
         memberFullName.text = Constants.LinkCommands.description[index]
         memberImage.image = UIImage(named: "about_kg_icon")
     }
