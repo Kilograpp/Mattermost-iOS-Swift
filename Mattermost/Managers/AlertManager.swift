741b4ef653de611d5f7e6b265934e6c7a1727b38
mehdzor <maxim@kilograpp.com>
2017-02-08 01:04:58 +0300

    Migration to SwiftMessages

--- Mattermost/Managers/AlertManager.swift
+++ Mattermost/Managers/AlertManager.swift
@@ -5,8 +5,7 @@
 //  Created by Julia Samoshchenko on 10.10.16.
 //  Copyright © 2016 Kilograpp. All rights reserved.
 //
-
-import Foundation
+import SwiftMessages
 
 //refactor this. temp functions
 enum AlertType {
@@ -18,18 +17,81 @@ enum AlertType {
 class AlertManager {
     static let sharedManager: AlertManager = AlertManager()
     
+    func showFileDownloadedAlert(fileIdentifier: String, tapHandler: @escaping (_ fileIdentifier: String) -> Void) {
+        let view = MessageView.viewFromNib(layout: .CardView)
+        
+        // Theme message elements with the warning style.
+        view.configureTheme(.success)
+        view.button?.setTitle("Open".localized, for: .normal)
+        view.configureDropShadow()
+        view.buttonTapHandler = { _ in
+            SwiftMessages.hide()
+            tapHandler(fileIdentifier)
+        }
+        
+        view.configureContent(title: "Success".localized, body: "File was successfully downloaded".localized)
+        SwiftMessages.show(view: view)
+    }
+    
+    func showTextCopyMessage() {
+        setupDefaultConfig()
+        let view = MessageView.viewFromNib(layout: .CardView)
+        view.configureTheme(.info)
+        view.configureDropShadow()
+        view.button?.isHidden = true
+        view.configureContent(title: "Info".localized, body: "Message copied to clipboard".localized)
+        SwiftMessages.show(view: view)
+    }
+    
+    func showLinkCopyMessage() {
+        setupDefaultConfig()
+        let view = MessageView.viewFromNib(layout: .CardView)
+        view.configureTheme(.info)
+        view.configureDropShadow()
+        view.button?.isHidden = true
+        view.configureContent(title: "Info".localized, body: "Link copied to clipboard".localized)
+        SwiftMessages.show(view: view)
+    }
+    
     func showSuccesWithMessage(message:String) {
-        let alertView = AlertView(type: .success, message: message)
-        alertView.showAlertView(animated: true)
+        setupDefaultConfig()
+        let view = MessageView.viewFromNib(layout: .CardView)
+        view.configureTheme(.success)
+        view.configureDropShadow()
+        view.button?.isHidden = true
+        view.configureContent(title: "Success".localized, body: message)
+        
+        SwiftMessages.show(view: view)
+
     }
     
     func showErrorWithMessage(message:String) {
-        let alertView = AlertView(type: .error, message: message)
-        alertView.showAlertView(animated: true)
+        setupDefaultConfig()
+        let view = MessageView.viewFromNib(layout: .CardView)
+        view.configureTheme(.error)
+        view.configureDropShadow()
+        view.button?.isHidden = true
+        view.configureContent(title: "Error".localized, body: message)
+        
+        SwiftMessages.show(view: view)
     }
     
     func showWarningWithMessage(message:String) {
-        let alertView = AlertView(type: .warning, message: message)
-        alertView.showAlertView(animated: true)
+        setupDefaultConfig()
+        let view = MessageView.viewFromNib(layout: .CardView)
+        view.configureTheme(.warning)
+        view.configureDropShadow()
+        view.button?.isHidden = true
+        view.configureContent(title: "Warning".localized, body: message)
+        
+        SwiftMessages.show(view: view)
+    }
+    
+    func setupDefaultConfig() {
+        SwiftMessages.defaultConfig.presentationStyle = .top
+        SwiftMessages.defaultConfig.presentationContext = .window(windowLevel: UIWindowLevelStatusBar)
+        SwiftMessages.defaultConfig.duration = SwiftMessages.Duration.seconds(seconds: 1.5)
+        SwiftMessages.defaultConfig.interactiveHide = true
+        SwiftMessages.defaultConfig.preferredStatusBarStyle = .lightContent
     }
 }
