0a7d5de728b3cedab147f3764c8dca84e3cba20b
Florin Peter <fp@datawerk.de>
2017-04-18 09:18:30 +0200

    fix relogin

--- Mattermost/ViewControllers/GitLabViewController.swift
+++ Mattermost/ViewControllers/GitLabViewController.swift
@@ -14,7 +14,8 @@ class GitLabViewController: UIViewController, UIWebViewDelegate {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var loader: UIView!
-    let titleName =  NSLocalizedString("GitLab", comment: "")
+    let titleName =  NSLocalizedString("Login with GitLab", comment: "")
+    var token = "";
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,12 +28,25 @@ class GitLabViewController: UIViewController, UIWebViewDelegate {
         super.didReceiveMemoryWarning()
     }
     
+    func webViewDidStartLoad(_ webView: UIWebView) {
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        if token != "" {
+            self.loadTeams();
+        }
+    }
+    
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         
             if let cookies = HTTPCookieStorage.shared.cookies {
                 for cookie in cookies {
                     if cookie.name == "MMAUTHTOKEN" {
-                        Api.sharedInstance.loginWithToken(cookie.value) { (error) in
+                        let topOffset = self.headerView.frame.height
+                        self.showLoaderView(topOffset: topOffset, bottomOffset: 0.0)
+                        
+                        token = cookie.value
+                        Api.sharedInstance.loginWithToken(token) { (error) in
                             guard (error == nil) else {
                                 let message = (error?.message == nil ? "an error occurred" : (error?.message))
                                 AlertManager.sharedManager.showErrorWithMessage(message: message!)
@@ -41,8 +55,6 @@ class GitLabViewController: UIViewController, UIWebViewDelegate {
                                 
                                 return
                             }
-                            
-                            self.loadTeams();
                         }
                     }
                 }
@@ -60,7 +72,13 @@ fileprivate protocol Setup {
 //MARK: Setup
 extension GitLabViewController: Setup {
     func initialSetup() {
+        reset()
         setupNavigationBar()
+    }
+    
+    func reset() {
+        UserStatusManager.sharedInstance.resetCookie();
+        token = ""
     }
     
     fileprivate func setupNavigationBar() {
@@ -76,7 +94,7 @@ extension GitLabViewController: Setup {
         navigationController.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.title = self.titleName
         self.setNeedsStatusBarAppearanceUpdate()
-    }    
+    }
 }
 
 fileprivate protocol Request {
@@ -90,20 +108,16 @@ extension GitLabViewController: Request {
     func login() {
         webView.delegate = self
         
-        let storage = HTTPCookieStorage.shared
-        for cookie in storage.cookies! {
-            storage.deleteCookie(cookie)
-        }
-        
         let url = URL(string: Preferences.sharedInstance.serverUrl! + "/api/v3/oauth/gitlab/login")
         let request = URLRequest(url: url!)
         
         webView.loadRequest(request)
+        hideLoaderView()
     }
     
     func loadTeams() {
         Api.sharedInstance.loadTeams(with: { (userShouldSelectTeam, error) in
-            guard (error == nil) else { self.hideLoaderView(); return }
+            guard (error == nil) else { self.hideLoaderView(); self.login(); return }
             
             if userShouldSelectTeam {
                 self.hideLoaderView()
@@ -122,7 +136,7 @@ extension GitLabViewController: Request {
     
     func loadTeamChannels() {
         Api.sharedInstance.loadChannels { (error) in
-            guard error == nil else { self.handleErrorWith(message: (error?.message)!); return }
+            guard error == nil else { self.handleErrorWith(message: (error?.message)!); self.login(); return }
             self.loadPreferedDirectChannelsInterlocuters()
         }
     }
@@ -133,22 +147,21 @@ extension GitLabViewController: Request {
         preferences.forEach{ usersIds.append($0.name!) }
         
         Api.sharedInstance.loadUsersListBy(ids: usersIds) { (error) in
-            guard error == nil else { self.handleErrorWith(message: (error?.message)!); return }
+            guard error == nil else { self.handleErrorWith(message: (error?.message)!); self.login(); return }
             self.loadTeamMembers()
         }
     }
     
     func loadTeamMembers() {
-        let predicate = NSPredicate(format: "identifier != %@ AND identifier != %@", Preferences.sharedInstance.currentUserId!,
-                                    Constants.Realm.SystemUserIdentifier)
+        let predicate = NSPredicate(format: "identifier != %@ AND identifier != %@", Preferences.sharedInstance.currentUserId!, Constants.Realm.SystemUserIdentifier)
         let users = RealmUtils.realmForCurrentThread().objects(User.self).filter(predicate)
         var ids = Array<String>()
         users.forEach{ ids.append($0.identifier) }
         
         Api.sharedInstance.loadTeamMembersListBy(ids: ids) { (error) in
-            guard error == nil else { self.handleErrorWith(message: (error?.message)!); return }
+            guard error == nil else { self.handleErrorWith(message: (error?.message)!); self.login(); return }
             Api.sharedInstance.getChannelMembers { (error) in
-                guard error == nil else { self.handleErrorWith(message: (error?.message)!); return }
+                guard error == nil else { self.handleErrorWith(message: (error?.message)!); self.login(); return }
                 DispatchQueue.main.async {
                     self.hideLoaderView()
                     RouterUtils.loadInitialScreen()
